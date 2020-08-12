5129f431d77de9fdef2a86ca82131577f10ef39f
Cole-Hayden <cwh8b4@gmail.com>
2020-08-12 13:42:47 -0500

    getToken func + hook to trigger getToken func

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Login from './Login';
+import { getTokenFromURL } from './spotify';
 
 function App() {
+  
+  // Run code based on a given condition
+  useEffect(() => {
+    const token = getTokenFromUrl();
+    console.log('I HAVE A TOKEN', token);
+  }, []);
+
   return (
     <div className="App">
       <h1>test</h1>
