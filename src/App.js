13bb303dc6c4d8076c791993e23392d9a58672cb
Cole-Hayden <cwh8b4@gmail.com>
2020-08-14 16:10:54 -0500

    setting up sidebar css + pulling playlists

--- src/App.js
+++ src/App.js
@@ -30,8 +30,13 @@ function App() {
         user: user
       });
     });
+    spotify.getUserPlaylists().then((playlists) => {
+      dispatch({
+        type: "SET_PLAYLISTS",
+        playlists: playlists
+      });
+    });
     }
-    console.log('I HAVE A TOKEN', _token);
   }, []);
   console.log('test', user);
   return (
