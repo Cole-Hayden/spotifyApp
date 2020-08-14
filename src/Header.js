f02870649314cfafc7d09d45b7c948eae8a54320
Cole-Hayden <cwh8b4@gmail.com>
2020-08-14 16:55:59 -0500

    adding header functionality in body

---
+++ src/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Header.css';
+import SearchIcon from "@material-ui/icons/Search";
+import { Avatar } from "@material-ui/core";
+
+function Header() {
+    return (
+        <div className="header">
+            <div className="header__left">
+                <SearchIcon />
+                <input placeholder="Search" type="text"/>
+
+            </div>
+            <div className="header__right">
+                <Avatar src="" alt="RQ" />
+                <h4>Rafeh Qazi</h4>
+            </div>
+        </div>
+    );
+}
+
+export default Header
